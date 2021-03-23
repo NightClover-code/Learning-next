539c5a27b23e0e16ea9322662e5c6498fa7eac57
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-23 21:20:20 +0100

    added detail page for individual ninja

--- pages/ninjas/[id].tsx
+++ pages/ninjas/[id].tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { Ninja } from '../../interfaces';
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -17,8 +17,32 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-const NinjaDetails = () => {
-  return <div>Details for ninjas</div>;
+export const getStaticProps: GetStaticProps = async context => {
+  const id = context.params?.id;
+  const { data }: { data: Ninja[] } = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  return {
+    props: {
+      ninja: data,
+    },
+  };
+};
+
+interface NinjaDetailsProps {
+  ninja: Ninja;
+}
+
+const NinjaDetails: React.FC<NinjaDetailsProps> = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
+    </div>
+  );
 };
 
 export default NinjaDetails;
