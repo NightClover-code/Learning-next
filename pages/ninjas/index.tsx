33e95c0abcdd5103cac9238df76674da960f85a2
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-23 20:52:34 +0100

    styled ninjas

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { GetStaticProps } from 'next';
 import { Ninja } from '../../interfaces';
+import styles from '../../styles/Ninjas.module.scss';
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data }: { data: Ninja[] } = await axios.get(
@@ -24,7 +25,7 @@ const NinjasPage: React.FC<NinjasPageInterface> = ({ ninjas }) => {
       <h1>Ninjas Page</h1>
       {ninjas.map(ninja => (
         <div key={ninja.id}>
-          <a>
+          <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
         </div>
