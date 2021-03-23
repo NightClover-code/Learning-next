fef4cffc86e395279493bcb8418f3bc85492f0f6
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-23 21:10:50 +0100

    using get static props

--- pages/ninjas/[id].tsx
+++ pages/ninjas/[id].tsx
@@ -1,3 +1,22 @@
+import axios from 'axios';
+import { GetStaticPaths } from 'next';
+import { Ninja } from '../../interfaces';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data }: { data: Ninja[] } = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  const paths = data.map(ninja => ({
+    params: { id: ninja.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const NinjaDetails = () => {
   return <div>Details for ninjas</div>;
 };
