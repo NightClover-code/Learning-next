25eb2777adaf67cf517d7da92d4f9e654465d94e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-23 17:09:28 +0100

    redirecting user after 3 seconds

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
       <p>&copy; Copyright 2021 Ninja List</p>
-    </div>
+    </footer>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.scss';
 
+const { title, text, btn } = styles;
+
 const HomePage: React.FC = () => (
   <div className="home__page">
-    <img src="" />
-    <h1>Hello Ninjas 👋</h1>
-    <p>
+    <h1 className={title}>Hello Ninjas 👋</h1>
+    <p className={text}>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat culpa quia
       neque fugit voluptatum tenetur aspernatur nam consequatur quod asperiores,
       possimus hic reiciendis, suscipit voluptates error temporibus consectetur
       rem expeditae
     </p>
-    <p>
+    <p className={text}>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est molestiae
       tenetur hic, fugit vero quasi, debitis illum, libero qui dolores labore
       laboriosam facilis iste! Earum labore totam ullam numquam assumenda?
     </p>
     <Link href="/ninjas">
-      <a className={styles.btn}>See Ninja Listing</a>
+      <a className={btn}>See Ninja Listing</a>
     </Link>
   </div>
 );
