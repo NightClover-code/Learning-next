2edef5bfab7eff13d6cbb505354e6d5500fe6d20
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-23 20:50:04 +0100

    data fetching with next

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,7 +1,34 @@
-const NinjasPage = () => {
+import axios from 'axios';
+import { GetStaticProps } from 'next';
+import { Ninja } from '../../interfaces';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data }: { data: Ninja[] } = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+interface NinjasPageInterface {
+  ninjas: Ninja[];
+}
+
+const NinjasPage: React.FC<NinjasPageInterface> = ({ ninjas }) => {
   return (
     <div>
       <h1>Ninjas Page</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
